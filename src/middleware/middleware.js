3f522af401c2403b5ad88152b02450ac10f964ed
Zaid <zedpx101@outlook.com>
2024-12-05 23:11:35 +0000

    Created hash password function

---
+++ src/middleware/middleware.js
@@ -0,0 +1,21 @@
+require ("dotenv").config();
+
+const bcrypt = require("bcrypt");
+
+async function hashPassword (req, res, next) {
+    try {
+        const saltRounds = parseInt(process.env.SALT);
+        const plainTextPassword = req.body.password;
+        const hashedPassword = await bcrypt.hash(plainTextPassword, saltRounds);
+        req.body.password = hashedPassword;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(501).json({
+            message: error.message,
+            error: error
+        });
+    };
+};
+
+hashPassword()
